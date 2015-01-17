5c7a919481aa78b462f5c9e081ec975208d25231
Aluriak <lucas.bourneuf@laposte.net>
2015-01-17 21:39:34 +0100

    basic code, need to be rewritted

---
+++ src/graph/graph.py
@@ -0,0 +1,185 @@
+# -*- encoding: utf-8 -*-
+from queue import Queue
+import pickle
+"""
+Graph representation:
+dict graph node:next
+dict weights node-next:weight
+"""
+
+# weight of a edge
+DISTANCE_BETWEEN_TWO_NODES = 1
+
+
+class Graph(object):
+    """Basic implementation of graph."""
+
+    def __init__(self, nodes, distanceBetweenTwoNodes = DISTANCE_BETWEEN_TWO_NODES):
+        self.nodes = nodes
+        self.first = True
+        self.distanceBetweenTwoNodes = distanceBetweenTwoNodes
+
+
+
+    def addSuccTo(self, thisNode, addedSuccessor):
+        """Add given successor to given node. Successor must be a list"""
+        # add addedSuccessor to node addedSuccessors
+        if self.nodes.get(thisNode) != None:
+            self.nodes[thisNode] += addedSuccessor
+        else:
+            self.nodes[thisNode] = addedSuccessor
+
+
+    def retSuccOf(self, thisNode, retiredSuccessor):
+        """Delete successor of given node. retiredSuccessor must be a list"""
+        if self.nodes.get(thisNode) != None:
+            # reconstruct the list without content 
+            # of retiredSuccessor
+            self.nodes[thisNode] = [x for x in self.nodes[thisNode] if x not in retiredSuccessor]
+        else:
+            pass # nothing to do
+
+
+
+    def __str__(self):
+        ret = ""
+        for node, succ in self.nodes.items():
+            ret += "("+str(node)+"): (" 
+            ret += ", ".join([str(x) for x in succ]) + ")\n"
+
+        return ret
+
+
+
+#Fonction Dijkstra (firstNode, target)
+    def Dijkstra(self, start, ends):
+        """Dijkstra. Return a list of duet.
+        Each duet is (path, evaluation) for a end node in ends argument"""
+        # PICKLE (for work offline with samples)
+        #fd = open("RECOVERY_GRAPH", "w")
+        #pickle.dump(self.nodes, fd)
+        #fd.close()
+
+
+        # for each node of graph
+        walked = {}
+        previous = {}
+        for key in self.nodes.iterkeys():
+            walked[key] = None # infinity
+            previous[key] = None
+            
+        # init walking dict and notFoundYet list
+        walked[start] = 0
+        notFoundYet = []
+        for key in self.nodes.iterkeys():
+            notFoundYet.append(key)
+
+        # while there is nodes not found
+        while len(notFoundYet) > 0:
+            minimalNode = self.nodeAtMinimumDistance(notFoundYet, walked)
+            #print "MINIMAL NODE POPPED: "+str(minimalNode)
+        
+            if minimalNode == None:
+                #print "\n\n======ASSERT\n"
+                #print "notFoundYet = "+str(notFoundYet)+"\n"
+                #print "\n",
+                #print "start = "+str(start)
+                #print "end = "+str(end)
+                #print "\n",
+                #for node in notFoundYet:
+                    #print str(node)+": "+str(walked[node])
+                #print "\n",
+                #print "walked = "+str(walked)+"\n"
+                #print "previous = "+str(previous)+"\n"
+                #print "\n\n======ASSERT\n\n"
+                exit(1)
+            # pasEncoreVu.enlever(n1)
+            # delete minimal from notFoundYet
+            tmp = []
+            for node in notFoundYet:
+                if node != minimalNode: tmp.append(node)
+            notFoundYet = tmp
+
+            # Pour n2 parcourant fils(n1)   // Les nœuds reliés à n1 par un arc
+            #     Si n2.parcouru > n1.parcouru + distance(n1, n2)   // distance correspond au poids de l'arc reliant n1 et n2
+            #         n2.parcouru = n1.parcouru + distance(n1, n2)
+            #         n2.précédent = n1   // Dit que pour aller à n2, il faut passer par n1
+            #     Fin si
+            # Fin pour
+            # for each successor of minimal
+            for successor in self.nodes[minimalNode]:
+                distance = self.distanceBetween(minimalNode, successor)
+                walkToSucc = walked[successor]
+                walkToNode = walked[minimalNode]
+                if walkToSucc == None    or    (walkToSucc > (walkToNode + distance)):
+                    walked[successor] = walkToNode + distance
+                    #print "DISTANCE"+str(distance)
+                    previous[successor] = minimalNode
+
+        # Here, all nodes are founded
+            
+
+
+        results = []
+        # for each end node
+        for end in ends:
+            #Fin tant que
+            #chemin = liste vide
+            #n = fin
+            #Tant que n != début
+            path = []
+            target = end
+            while target != start:
+                # chemin.ajouterAvant(n)
+                # n = n.précédent
+                path = [target] + path
+                target = previous[target]
+
+            # DEBUG
+            if self.first:
+                limit = range(self.jacky.game.board.size)
+                for row in limit:
+                    for col in limit:
+                        if walked.has_key((row,col)):
+                            print(str(walked[(row,col)]), "\t|",)
+                        else:
+                            print("%%%\t|")
+                    #print "\n",
+                print("====================================")
+                self.first = False
+
+            #Fin tant que
+            #chemin.ajouterAvant(début)
+            #Retourner chemin
+            path = [start] + path
+            evaluation = walked[path[-1]]
+            results.append((path, evaluation))
+
+        # return
+        return results
+    # END Dijkstra
+
+
+    def nodeAtMinimumDistance(self, notFoundYet, distances):
+        """Wait for non-empty list of tuple (x,y), keys of distances dictionnary.
+        Return the key with the minimum positive value"""
+        # found minimal
+        minimal = None
+        for node in notFoundYet:
+            if distances[node] != None: 
+                if minimal == None or (distances[minimal] > distances[node]):
+                        minimal = node
+
+        # return
+        return minimal
+
+
+
+    def distanceBetween(self, node1, node2):
+       """Wait two nodes. Return the minimum distance between these two nodes"""
+       return self.distanceBetweenTwoNodes
+
+
+
+
+
